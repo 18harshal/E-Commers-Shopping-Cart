b0f1c3aa6d049effab01d462220c2e3c0bb8c882
Harshal Patil <18harshalp@gmail.com>
2025-01-10 12:31:51 +0530

    e- Commers cart System

--- src/main/java/com/company/model/Employee.java
+++ src/main/java/com/company/model/Employee.java
@@ -7,10 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @Entity
 @Table(name="employees")
+@Getter
+@Setter
 public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
